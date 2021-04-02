10fdddc31526610d93487ad5d90b2efd7c702da3
David Botros <a01052944@unet.univie.ac.at>
2021-04-02 22:27:30 +0200

    +allauth auth method

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Steps :
 
-- create a virtual env
+- create a virtualenv (virtual env)
 - install the requirments in the requirments.txt (pip install -r requirments.txt)
 - change the project name using the rename function (python manage.py rename project_name) only usable for one time
+- run (python manage.py collectstatic)
+-create a (static_in_env) folder in the BASE_DIR
--- e_commerce/settings/base.py
+++ e_commerce/settings/base.py
@@ -17,6 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     'core',
 ]
 
@@ -72,3 +78,9 @@ STATIC_ROOT = os.path.join(VENV_PATH,'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(VENV_PATH,'media')
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
